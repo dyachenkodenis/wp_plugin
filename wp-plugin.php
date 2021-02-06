72d04220092504d9c18ae0666d666dbe3df286b8
dyachenkodenis <denisdyachenko.90@gmail.com>
2021-02-06 09:03:13 +0500

    добавил файлы в плагин

---
+++ wp-plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Basic Wordpress Plugin
+ * 
+ * @package           Basic Wordpress Plugin Package
+ * @author            
+ * @version 
+ * @copyright         
+ * @license   
+ * @link        
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Basic Wordpress Plugin
+ * Plugin URI:        
+ * Description:       
+ * Version:           1.0.0
+ * Requires at least: 
+ * Requires PHP:      
+ * Author:            
+ * Author URI:        
+ * Text Domain:       
+ * License:           
+ * License URI:       
+ */
